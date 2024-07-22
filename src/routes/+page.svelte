c36104a4e9aca8ec0ca0893b3c2717fef869a6bd
Traw <kirito.san1253@gmail.com>
2024-07-22 23:10:37 +0300

    day,month,year

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import "dayjs/locale/ar";
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	dayjs.locale("ar");
+
+	let preiod = dayjs().format("a") == "م" ? "مساء" : "صباح";
+</script>
+
+<div class="p-16">
+	<header class="flex justify-between">
+		<div>
+			<h1 class="text-4xl mb-2"> {preiod} الخير 👋 </h1>
+			<h2 class="text-2xl text-surface-900/50"> 
+				{dayjs().format("اليوم ، dddd D MMM YYYY")} 
+			</h2>
+		</div>
+		<div class="bg-white w-32 h-14 "></div>
+	</header>
 </div>
