d37721cd55e3b609ea6943e736f34eab947c4a2c
Traw <kirito.san1253@gmail.com>
2024-07-31 17:33:25 +0300

    pwa tweak

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,18 @@
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
   import { initializeStores, Modal } from '@skeletonlabs/skeleton';
+  //@ts-ignore
+  import { pwaInfo } from 'virtual:pwa-info'; 
+
 
 initializeStores();
+
+$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
+
+<svelte:head> 
+ 	{@html webManifestLink} 
+</svelte:head>
 
 <Modal buttonPositive="variant-filled-error"/>
 <slot />
